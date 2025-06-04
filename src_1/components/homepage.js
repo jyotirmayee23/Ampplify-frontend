d28b6dc2a4f12f3052468e3230dad0f1a564fb73
parthraj-mori-1 <parthrajmori2003@gmail.com>
2025-06-04 15:48:32 +0530

    changed ui code

---
+++ src_1/components/homepage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, Container, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import '../styles/app.css';
+
+const HomePage = () => {
+  const applications = [
+    { name: 'Home Healthcare Referral Summary', path: '/home-healthcare' },
+    { name: 'Invoice Processing', path: '/invoice-processing' },
+    { name: 'Chat with PDF', path: '/chat-pdf' },
+    { name: 'Immigration Information Extraction', path: '/immigration-info' },
+    { name: 'Damage Detection', path: 'http://43.205.185.17:5002/' },
+  ];
+
+  return (
+    <Container maxWidth="md">
+      <div className="home-wrapper">
+        <Typography variant="h4" className="title">
+          📂 Operisoft GenAI Projects
+        </Typography>
+        <Typography variant="h6" className="subtitle">
+          Select an Application
+        </Typography>
+        <div className="button-container">
+          {applications.map((app, index) => (
+            <Button
+              key={index}
+              variant="contained"
+              component={Link}
+              to={app.path}
+              className="app-button"
+            >
+              {app.name}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
